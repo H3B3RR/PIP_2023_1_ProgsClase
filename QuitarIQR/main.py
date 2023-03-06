84c765125e938081f5f4d6e45dac0c5ca324e540
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2023-03-06 11:53:16 -0600

    Ultima Clase

---
+++ QuitarIQR/main.py
@@ -0,0 +1,5 @@
+
+
+linea = "Fpppp-8-334-0.1-0.5 &$6.63E2_{0.0E0}\blacktriangle$ &$6.38E2_{3.3E1}\blacktriangle$ &$6.03E2_{1.3E1}\blacktriangle$ &\cellcolor{gray25}$6.01E2_{9.0E0}\blacktriangle$ &$6.09E2_{0.0E0}\blacktriangle$ &\cellcolor{gray95}$5.62E2_{6.3E0}$"
+
+print(linea)
